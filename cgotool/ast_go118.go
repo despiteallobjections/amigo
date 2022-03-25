fd8e56cd5deb23a0c08545a47116b88b8e5761a4
Matthew Dempsky <mdempsky@google.com>
2022-03-25 12:16:25 -0700

    amigo/cgotool: use go1.18 build tag instead of compiler_bootstrap

--- cgotool/ast_go118.go
+++ cgotool/ast_go118.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !compiler_bootstrap
-// +build !compiler_bootstrap
+//go:build go1.18
 
 package main
 
